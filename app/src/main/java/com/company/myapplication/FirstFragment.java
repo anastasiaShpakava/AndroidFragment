bdf579b23e4e08cd85030c11df1eb8cb8f4cde3c
anastasiaShpakava <shpakavaa@gmail.com>
2020-10-01 20:43:11 +0300

    added Toast object

--- app/src/main/java/com/company/myapplication/FirstFragment.java
+++ app/src/main/java/com/company/myapplication/FirstFragment.java
@@ -1,9 +1,14 @@
 package com.company.myapplication;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +19,16 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment, container, false);
+        Button button = rootView.findViewById(R.id.button);
+        EditText editText = rootView.findViewById(R.id.editTextTextPersonName);
+        final Editable enteredText = editText.getText();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getView().getContext(), enteredText, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
         return rootView;
     }
 }
