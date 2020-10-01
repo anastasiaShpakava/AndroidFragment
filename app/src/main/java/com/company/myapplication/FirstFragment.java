92c6f79d9105e647dc40dd1a137e06255349f992
anastasiaShpakava <shpakavaa@gmail.com>
2020-10-01 21:09:07 +0300

    added TextWatcher object

--- app/src/main/java/com/company/myapplication/FirstFragment.java
+++ app/src/main/java/com/company/myapplication/FirstFragment.java
@@ -2,12 +2,14 @@ package com.company.myapplication;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,13 +17,36 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FirstFragment extends Fragment {
+    private EditText editText;
+    private TextView textView;
+    private Button button;
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String text = editText.getText().toString();
+            textView.setText("You entered: " + text);
+        }
+    };
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment, container, false);
-        Button button = rootView.findViewById(R.id.button);
-        EditText editText = rootView.findViewById(R.id.editTextTextPersonName);
+        button = rootView.findViewById(R.id.button);
+        textView = rootView.findViewById(R.id.textView);
+        editText = rootView.findViewById(R.id.editTextTextPersonName);
         final Editable enteredText = editText.getText();
+        editText.addTextChangedListener(textWatcher);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +54,7 @@ public class FirstFragment extends Fragment {
                 toast.show();
             }
         });
+
         return rootView;
     }
 }
